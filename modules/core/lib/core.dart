a0e5506d1a3439386f21f24f841aa6cf56e00c82
solano_furlan <solanofurlan@outlook.com>
2022-03-17 15:44:49 +0000

    Firebase authentication

--- modules/core/lib/core.dart
+++ modules/core/lib/core.dart
@@ -1,3 +1,4 @@
 library core;
 
 export 'default_values/backend_mock_values.dart';
+export 'enums/auth_enum.dart';
--- modules/domain/lib/domain.dart
+++ modules/domain/lib/domain.dart
@@ -3,4 +3,5 @@ library domain;
 export 'article/article.filter.dart';
 export 'article/article.repository.dart';
 export 'article/article.entity.dart';
-export 'remote_config/remote_config_repo.dart';
+export 'remote_config/remote_config.repo.dart';
+export 'authentication/authentication.repo.dart';
